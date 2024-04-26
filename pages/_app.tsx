d2eabc66661d1fce84fe35b4f1c5fd3aad2cac80
Lanre Akintayo <akintayolanre2019@gmail.com>
2024-04-26 15:51:24 +0100

    Updated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,30 +7,62 @@ import {
   w3mConnectors,
   w3mProvider,
 } from "@web3modal/ethereum";
-import {  avalanche, zkSync, sepolia } from "wagmi/chains";
+import { avalanche, zkSync, sepolia } from "wagmi/chains";
 import { Web3Modal } from "@web3modal/react";
 import { configureChains, createConfig, WagmiConfig } from "wagmi";
 import ProposalsProvider from "../providers/ProposalsProvider";
 import { NextPageWithLayout } from "../types";
+import { webSocket, createPublicClient, http } from "viem";
+
+import "@rainbow-me/rainbowkit/styles.css";
+
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import "@rainbow-me/rainbowkit/styles.css";
+import { publicProvider } from "wagmi/providers/public";
+
+// https://github.com/wevm/viem/issues/923
 
 if (!process.env.NEXT_PUBLIC_PROJECT_ID) {
   throw new Error("You need to provide NEXT_PUBLIC_PROJECT_ID env variable");
 }
 const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
+const rpcUrl = process.env.NEXT_PUBLIC_RPC_URL;
 
-// 2. Configure wagmi client
-const chains = [sepolia];
+// // 2. Configure wagmi client
+// const chains = [sepolia];
 
-const { publicClient } = configureChains(chains, [w3mProvider({ projectId })]);
+// const { publicClient } = configureChains(chains,  [w3mProvider({ projectId })]);
+
+// const wagmiConfig = createConfig({
+//   autoConnect: true,
+//   connectors: w3mConnectors({ chains, projectId }),
+//   publicClient
+// });
+
+// // 3. Configure modal ethereum client
+// const ethereumClient = new EthereumClient(wagmiConfig, chains);
+
+//////////
+const { chains } = configureChains([sepolia], [publicProvider()]);
+
+const publicClient = createPublicClient({
+  chain: sepolia,
+  transport: http(rpcUrl),
+});
+
+const { connectors } = getDefaultWallets({
+  appName: "Dao Governance",
+  projectId: projectId,
+  chains,
+});
 
 const wagmiConfig = createConfig({
   autoConnect: true,
-  connectors: w3mConnectors({ chains, projectId }),
-  publicClient,
+  connectors,
+  publicClient
 });
 
-// 3. Configure modal ethereum client
-const ethereumClient = new EthereumClient(wagmiConfig, chains);
+///////////
 
 type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
@@ -40,16 +72,14 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page: any) => page);
 
   return (
-    <ProposalsProvider>
-        <WagmiConfig config={wagmiConfig}>
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider coolMode chains={chains}>
+        <ProposalsProvider>
           {getLayout(<Component {...pageProps} />)}
-        </WagmiConfig>
-
-        <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
-
-        <ToastContainer />
-      {/* </NotificationProvider> */}
-    </ProposalsProvider>
+          <ToastContainer />
+        </ProposalsProvider>
+      </RainbowKitProvider>
+    </WagmiConfig>
   );
 }
 
